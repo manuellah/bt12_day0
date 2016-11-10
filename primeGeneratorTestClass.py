a32424707c5894b0ae2d46e1714617bc48de611c
manuellah <emmanumuthui@gmail.com>
2016-11-10 16:15:43 +0300

    Done with 10 Test

--- primeGeneratorTestClass.py
+++ primeGeneratorTestClass.py
@@ -11,18 +11,28 @@ class TestPrimeGenerator(unittest.TestCase):
             prime_generator("test")
             
     def test_return_list_for_valid_input(self):
+        self.assertIsInstance(prime_generator(8), list)
         
     def test_float_nums(self):
-        pass
+        with self.assertRaises(TypeError):
+            prime_generator(9.23)
+            
+    def test_list_argument(self):
+         with self.assertRaises(TypeError):
+            prime_generator([2, 5, 7])
+            
     def test_negative_numbers(self):
         self.assertEqual(prime_generator(-12), "The number has to be greater than 1")
-    def test_numbers_less_then_two(self):
-        pass
+        
+    def test_numbers_less_than_two(self):
+         self.assertEqual(prime_generator(0), "The number has to be greater than 1")
+        
     def test_with_one(self):
         self.assertEqual(prime_generator(1), "The number has to be greater than 1")
     
     def test_with_two(self):
         self.assertEqual(prime_generator(2), [2])
+        
     def test_with_three(self):
         self.assertEqual(prime_generator(3), [2, 3])
     
