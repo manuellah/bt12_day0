04f48dc4924fe5563ce0a6cb2231d726265738ea
manuellah <emmanumuthui@gmail.com>
2016-11-10 21:12:54 +0300

    refactored my code

--- primegenerator.py
+++ primegenerator.py
@@ -5,20 +5,17 @@ def prime_generator(n):
     elif n < 2:
         return "The number has to be greater than 1"
     
-    elif n == 2:
-        return [2]
     
-    elif n == 3:
-        return [2, 3]
-    
-    prime_nums = [2]
-    for num in range(3, n+1):
+    prime_nums = [2]    
+    for num in range(2, n+1):
+        #optimized with trial division and added two due to
+        #truncation effect of int() function
         for divisor in range(2, int(num**(1/2.0) + 2)):
             if num % divisor == 0:
                 break
         else:
             prime_nums.append(num)
-            
+
     return  prime_nums
 
-print prime_generator(21)
+print prime_generator(15)
