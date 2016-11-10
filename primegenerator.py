a3933164bc34908cebe277df39f8cecec29766f0
manuellah <emmanumuthui@gmail.com>
2016-11-10 20:49:34 +0300

    optimized my function

--- primegenerator.py
+++ primegenerator.py
@@ -13,10 +13,12 @@ def prime_generator(n):
     
     prime_nums = [2]
     for num in range(3, n+1):
-        for divisor in range(2, num):
+        for divisor in range(2, int(num**(1/2.0) + 2)):
             if num % divisor == 0:
                 break
         else:
             prime_nums.append(num)
             
     return  prime_nums
+
+print prime_generator(21)
