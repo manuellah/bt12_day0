8e2887e50d9c80a7a935dfbc7feb3ef42addd081
manuellah <emmanumuthui@gmail.com>
2016-11-10 15:03:07 +0300

    added prime_generator

--- primegenerator.py
+++ primegenerator.py
@@ -1,2 +1,24 @@
-def prime_generator():
-    pass
+def prime_generator(n):
+    if not isinstance(n, int):
+        return "Invalid Input"
+    
+    elif n < 2:
+        return "The number has to be greater than 1"
+    
+    elif n == 2:
+        return [2]
+    
+    elif n == 3:
+        return [2, 3]
+    
+    prime_nums = [2]
+    for num in range(3, n+1):
+        for divisor in range(2, num):
+            if num % divisor == 0:
+                break
+        else:
+            prime_nums.append(num)
+            
+    return  prime_nums
+
+print prime_generator(13)
